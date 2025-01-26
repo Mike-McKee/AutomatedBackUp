c0afcd2245658bba8d245df2879a03b21610df4b
Mike McKee <mike@dorkydata.com>
2025-01-26 11:58:09 -0500

    Renamed file

--- scripts/LocalizePowershellFunctions.ps1
+++ scripts/LocalizePowershellFunctions.ps1
@@ -28,7 +28,7 @@ function AddAsSourcePath {
     `$CurrentDirectory = (Get-Location).Path.ToString()
 
     #Set path to config.json
-    `$JsonPath = '$ConfigPath'
+    `$JsonPath = 'C:\Users\mike\Coding\AutomatedBackup\'
 
     # First, if no parameter passed, add current Directory to config.json
     if (-not `$DirToAdd) {
@@ -75,6 +75,36 @@ function AddAsSourcePath {
     }
 }
 # End AddAsSourcePath
+"@
+
+$ExecuteAutomatedBackup = @"
+function ExecuteAutomatedBackup {
+# First, let's go to the directory where our Python script exists
+<# ---------------- INSERT THE REPO'S DIRECTORY BELOW ---------------- #>
+cd 'C:\Users\mike\Coding\AutomatedBackup\'
+
+if (-not (test-path ".\myenv\")) {
+    <# Initialize Python virtual environment and install necessary libraries #>
+    write-host "Creating Python Virtual Environment..."
+    # Initialize virtual 
+    py -m venv myenv
+    write-host "Python Virtual Environment 'myenv' Created"
+
+    # Activate virtual environment
+    .\myenv\Scripts\Activate
+
+    # install necessary pip libraries
+    pip install -r requirements.txt
+} else {
+    & .\myenv\Scripts\Activate
+}
+
+# NOW WE EXECUTE THE PYTHON BACKUP SCRIPT
+py .\AutomatedBackUp.py
+
+write-host "YAY!!! FINISHED"
+deactivate
+}
 "@
 
 # Now add function to current session
